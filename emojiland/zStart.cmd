2575c332b78ed69e82bf1af28894744eff0b1e60
RGJ <115826702+RGJP@users.noreply.github.com>
2026-02-22 20:06:24 -0500

    bp
    bp

--- emojiland/zStart.cmd
+++ emojiland/zStart.cmd
@@ -1,4 +1,4 @@
-taskkill /F /IM chrome.exe /IM msedge.exe /IM firefox.exe /IM opera.exe /IM brave.exe
+taskkill /F /IM chrome.exe /IM msedge.exe
 timeout /t 2 
 start http://localhost:8000/index.html
 python -m http.server
